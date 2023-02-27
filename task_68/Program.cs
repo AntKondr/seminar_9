c0e03e9e02604896bb22e1cc9093508075b4b5ea
Anton_Kondratev <elkondro@gmail.com>
2023-02-27 13:00:06 +0700

    task_68 solution

--- task_68/Program.cs
+++ task_68/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("seminar_9 task_68\n\n");
+
+int ack(int m, int n){
+    if (m == 0){
+        return n + 1;
+    }
+    else if (n == 0){
+        return ack(m - 1, 1);
+    }
+    else {
+        return ack(m - 1, ack(m, n - 1));
+    }
+}
+
+
+Console.Write("введите M и N через пробел: ");
+int [] mn = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+Console.Write(ack(mn[0], mn[1]));
